9c02d216f4eb33015af211e34d7ed53bbd862192
hayleyxyo <haiqianzhang@HAIQIANs-MacBook-Air.local>
2017-01-05 20:28:12 +1100

    Comment added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  $("form").submit(function(event){
-    event.preventDefault();
+  $("form").submit(function(event){ // when you click or enter a value
+    event.preventDefault();// provent the browser to refresh the website
 
-    var city = $("#city-type").val().toLowerCase(); //.tolowercase 会让你不管输入NCY还是Ncy还是 ncy都会work
+    var city = $("#city-type").val().toLowerCase(); //.tolowercase no matter you enter NYC or nyc, they both worked
     displayCity(city);
   });
 
@@ -13,19 +13,19 @@ $(document).ready(function(){
       $("body").attr("class", "nyc"); //第一个"class" 的意思是把"body" attribute to "class", 第二个""代表这个class的名字
     }
 
-    else if (city === "austin" || city === "Austin" || city ==="ATX") {
+    else if (city === "austin" || city === "Austin" || city ==="ATX") { //no matter user enter "austin", "ATX" or "austin", picture of Austin will come out
       $("body").attr("class", "austin");
     }
 
-    else if (city === "sf" || city === "Bay Area" || city === "San Francisco") {
+    else if (city === "sf" || city === "Bay Area" || city === "San Francisco") { //no matter user enter "sf", "Bay Area" or "San Francisco", picture of Austin will come out
       $("body").attr("class", "sf");
     }
 
-    else if (city ==="la" || city ==="LAX" || city ==="Los Angeles") {
+    else if (city ==="la" || city ==="LAX" || city ==="Los Angeles") { //no matter user enter "la", "Los Angeles" or "LAX", picture of Austin will come out
       $("body").attr("class", "la")
     }
 
-    else if (city ==="syd" || city ==="Sydney" || city === "SYD") {
+    else if (city ==="syd" || city ==="Sydney" || city === "SYD") { //no matter user enter "syd", "sydney" or "SYD", picture of Austin will come out
       $("body").attr("class", "sydney")
     }
   }
